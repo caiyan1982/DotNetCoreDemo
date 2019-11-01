990e6848ebfd9fb26dd848c1728142dd3cd639ae
caiy <caiyan1982@hotmail.com>
2019-11-01 17:36:05 +0800

    新增简单将日志写入不同目的地的示例。

---
+++ Logging/LoggingDemo1/Demo1.cs
@@ -0,0 +1,26 @@
+﻿/*********
+* 
+*   from:https://www.cnblogs.com/artech/p/logging-for-net-core-01.html
+*   
+*********/
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
+
+namespace LoggingDemo1
+{
+    class Demo1
+    {
+        public void Execute()
+        {
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            loggerFactory.AddProvider(new DebugLoggerProvider());
+            ILogger logger = loggerFactory.CreateLogger(nameof(Program));
+
+            int eventId = 123;
+
+            logger.LogInformation(eventId, "升级到最新.Net Core版本({version})", "1.0.0");
+            logger.LogWarning(eventId, "并发量接近上限({maximum})", 200);
+            logger.LogError(eventId, "数据库连接失败(数据库：{Database}，用户名：{User})", "TestDb", "sa");
+        }
+    }
+}
---
+++ Logging/LoggingDemo1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LoggingDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo1 demo1 = new Demo1();
+            demo1.Execute();
+
+            Console.Read();
+        }
+    }
+}
