93aa655afa7f9e2ee177a2df8095a6a9bb17e288
caiy <caiyan1982@hotmail.com>
2019-11-04 18:03:57 +0800

    新增控制台日志输出示例。

---
+++ Logging/LoggingDemo1/ConsoleLoggerDemo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggingDemo1
+{
+    class ConsoleLoggerDemo
+    {
+        public void Execute()
+        {
+            IConfiguration loggerConfig = new ConfigurationBuilder()
+                .AddJsonFile("logging.json")
+                .Build();
+
+            ConsoleLoggerOptions loggerOptions = new ServiceCollection()
+                .AddOptions()
+                .Configure<ConsoleLoggerOptions>(loggerConfig)
+                .BuildServiceProvider()
+                .GetService<IOptions<ConsoleLoggerOptions>>()
+                .Value;
+
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder 
+                => builder.AddConfiguration(loggerConfig).AddConsole());
+            ILogger logger = loggerFactory.CreateLogger("App4");
+            int eventId = 123;
+
+            logger.LogInformation(eventId, "升级到最新.Net Core版本({version})", "1.0.0");
+            logger.LogWarning(eventId, "并发量接近上限({maximum})", 200);
+            logger.LogError(eventId, "数据库连接失败(数据库：{Database}，用户名：{User})", "TestDb", "sa");
+        }
+    }
+}
--- Logging/LoggingDemo1/Program.cs
+++ Logging/LoggingDemo1/Program.cs
@@ -6,8 +6,11 @@ namespace LoggingDemo1
     {
         static void Main(string[] args)
         {
-            Demo1 demo1 = new Demo1();
-            demo1.Execute();
+            //Demo1 demo1 = new Demo1();
+            //demo1.Execute();
+
+            ConsoleLoggerDemo consoleLoggerDemo = new ConsoleLoggerDemo();
+            consoleLoggerDemo.Execute();
 
             Console.Read();
         }
